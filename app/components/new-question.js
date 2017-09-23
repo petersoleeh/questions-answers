629e0ab92fda77f8c69acb9a54add8d82e48040a
peter soleeh <petersoleeh@gmail.com>
2017-09-23 19:27:00 +0300

    adding borders

---
+++ app/components/new-question.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    qformShow() {
+      this.set('addNewQuestion', true)
+    },
+    saveQuestion1() {
+      var params = {
+        title: this.get('title') || "",
+        author: this.get('author') || "",
+        answer: this.get('answer') || "",
+        date: this.get('date') || ""
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion2', params);
+    }
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -43,5 +43,12 @@ var questions = [{
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
+  },
+  actions: {
+    saveQuestion3(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    }
   }
 });
